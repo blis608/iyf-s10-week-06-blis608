479f46966be8b3318f504173492c87bb185924f9
Irene <blissnjoki552@gmail.com>
2026-03-31 14:40:59 +0300

    Refactor user display and search functionality

--- main.js
+++ main.js
@@ -3,46 +3,42 @@ const searchInput = document.getElementById("search");
 
 let users = [];
 
+// Fetch users from API
 async function fetchUsers() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   users = await response.json();
   displayUsers(users);
 }
 
+// Display users on the page
 function displayUsers(userList) {
-
   userContainer.innerHTML = "";
 
-  userList.forEach(function(user){
-
+  userList.forEach(function(user) {
     const card = document.createElement("div");
     card.classList.add("user-card");
 
     card.innerHTML = `
-<h3>${user.name}</h3>
-<p>Email: ${user.email}</p>
-<p>Company: ${user.company.name}</p>
-<p>City: ${user.address.city}</p>
-`;
+      <h3>${user.name}</h3>
+      <p>Email: ${user.email}</p>
+      <p>Company: ${user.company.name}</p>
+      <p>City: ${user.address.city}</p>
+    `;
 
     userContainer.appendChild(card);
-
   });
-
 }
 
-searchInput.addEventListener("keyup", function(){
-
+// Search users
+searchInput.addEventListener("keyup", function() {
   const searchValue = searchInput.value.toLowerCase();
 
-  const filteredUsers = users.filter(function(user){
-
+  const filteredUsers = users.filter(function(user) {
     return user.name.toLowerCase().includes(searchValue);
-
   });
 
   displayUsers(filteredUsers);
-
 });
 
+// Load users
 fetchUsers();
